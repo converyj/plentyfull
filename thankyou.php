3fe438197b5546108b52da817987f0d8514bed6b
CaptainAries <captainariesz@gmail.com>
2018-11-18 18:55:58 -0500

    thankyou page-Aries

---
+++ thankyou.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Plenty Full - Thank you!</title>
+</head>
+<body>
+<nav>
+  <a href="explore.php">Explore</a> |
+  <a href="process_inputcode.php">Input Code</a> |
+  <a href="about.php">About</a> |
+  <a href="login.php">Login</a>
+</nav>
+
+  <h1>Thank You!</h1>
+
+</body>
+
+
+<footer>
+  <a href="mailto:info@plentyfull.com">info@plentyfull.com</a>
+  <br />
+  <a href="https://www.instagram.com/"><img src="images/instagram.png" alt="ins" /></a>
+  <a href="https://www.facebook.com/"><img src="images/facebook.png" alt="facebook" /></a>
+  <a href="https://www.twitter.com/"><img src="images/twitter.png" alt="twitter" /></a>
+
+</footer>
+</html>
