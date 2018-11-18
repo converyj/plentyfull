d16b1bf09740878695c9f3ceb1d01d5d8c2c4e27
CaptainAries <captainariesz@gmail.com>
2018-11-18 18:11:26 -0500

    ty&share code page-Aries

---
+++ thanku-sharecode.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Plenty Full - Thank you! Now share your code</title>
+</head>
+<body>
+  <nav>
+  <a href="explore.php">Explore</a> |
+  <a href="inputCode.php">Input Code</a> |
+  <a href="about.php">About</a> |
+  <a href="login.php">Login</a>
+</nav>
+  <h1>Plenty Full</h1>
+  <h2>Your unique code is: </h2>
+  <h2>iwkhuy4</h2>
+  <h3>When your ready to view results you can input this code through the input code page in our menu.</h3>
+  <h3>Here Is your survey link.Share this with your attendees !</h3>
+  <h3>
+    <a href="http://plentyfull.com/iwkhuy4">http://plentyfull.com/iwkhuy4</a>
+  </h3>
+  <p>
+    <a href="register.php">Would you like to register for our site?</a>
+  </p>
+</body>
+
+
+<footer>
+  <a href="mailto:info@plentyfull.com">info@plentyfull.com</a>
+  <br />
+  <a href="https://www.instagram.com/"><img src="images/instagram.png" alt="ins" /></a>
+  <a href="https://www.facebook.com/"><img src="images/facebook.png" alt="facebook" /></a>
+  <a href="https://www.twitter.com/"><img src="images/twitter.png" alt="twitter" /></a>
+
+</footer>
+</html>
