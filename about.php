c27fffea62ab1af132665aac12687e35c30fcfad
CaptainAries <captainariesz@gmail.com>
2018-11-18 19:24:23 -0500

    about page-Aries

---
+++ about.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Plenty Full - About</title>
+</head>
+<body>
+<nav>
+  <a href="explore.php">Explore</a> |
+  <a href="inputCode.php">Input Code</a> |
+  <a href="about.php">About</a> |
+  <a href="login.php">Login</a>
+</nav>
+
+  <h1>About Us</h1>
+  <p>
+    EJAX will be creating a web application called “Plenty Full”. Our application is used to help match people or organizations up with inclusive catering options close by that best suits the needs of everyone involved. Our goal is to make the catering options at events more inclusive no matter what the dietary or allergen restrictions are.
+  </p>
+  <h2>Amanda Goncalves</h2>
+  <p>
+    Originally from the Niagara region I moved to Oakville this year to study at Sheridan. My undergrad was in graphic design and I have worked in the industry for two years before realizing that I am more interested in learning UX/UI and web development. Outside of school and work life I enjoy reading and painting.
+  </p>
+  <h2>Elicia Ng</h2>
+  <p>
+    Elicia was born in Scarborough and has lived most of her life in Markham. Despite coming from an accounting and financial management undergraduate background, Elicia is currently attending Sheridan College in hopes of utilizing both skill sets for future endeavors. In her spare time, she enjoys illustrating and meeting up with friends.
+  </p>
+  <h2>Jaime Convery</h2>
+  <p>
+    I was born in Toronto but moved to Oakville when I was young and have lived here ever since. I am a Sheridan College student working towards becoming a Front-End Web Developer. I have a diploma in Computer Programming.
+    In my spare time I enjoy hanging out with family, reading books, and coding.
+  </p>
+  <h2>Aries Zhang</h2>
+  <p>
+    My name is Xiaochun Zhang and you can just call me Aries. I was born in China and I graduated from Communication University of China with a major in animation. I also studied graphic design at Arts University Bournemouth in the UK for one year. I am now studying interactive media at Sheridan College in Ontario, Canada. I love playing baseball and softball in my spare time.
+  </p>
+
+</body>
+
+
+<footer>
+  <a href="mailto:info@plentyfull.com">info@plentyfull.com</a>
+  <br />
+  <a href="https://www.instagram.com/"><img src="images/instagram.png" alt="ins" /></a>
+  <a href="https://www.facebook.com/"><img src="images/facebook.png" alt="facebook" /></a>
+  <a href="https://www.twitter.com/"><img src="images/twitter.png" alt="twitter" /></a>
+
+</footer>
+</html>
