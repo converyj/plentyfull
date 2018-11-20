95b00e660f5a915166b51028e0e0f1d90f443afb
Jaime <jaimeconvery@gmail.com>
2018-11-20 17:50:48 -0500

    login.php-v2

--- login.php
+++ login.php
@@ -8,31 +8,26 @@
 </head>
 <body>
 <nav>
-  <a href="explore.php">Explore</a> |
-  <a href="inputCode.php">Input Code</a> |
-  <a href="about.php">About</a> |
+  <!--  not proper <ul><li> -->
+  <!-- plentyfull link on nav -->
+  <a href="explore.php">Explore</a> 
+  <a href="inputCode.php">Input Code</a> 
+  <a href="about.php">About</a> 
   <a href="login.php">Login</a>
 </nav>
 
 <form action="process_login.php" method="POST">
-  <fieldset>
-    <legend>Log In</legend>
+    <h1>Log In</h1>
     Email:<input type="email" name="email" /><br />
     Password:<input type="password" name="password" required="required" /><br />
     <input type="submit" value="Go" />
-
-  </fieldset>
 </form>
-
 </body>
-
-
 <footer>
   <a href="mailto:info@plentyfull.com">info@plentyfull.com</a>
   <br />
   <a href="https://www.instagram.com/"><img src="images/instagram.png" alt="ins" /></a>
   <a href="https://www.facebook.com/"><img src="images/facebook.png" alt="facebook" /></a>
   <a href="https://www.twitter.com/"><img src="images/twitter.png" alt="twitter" /></a>
-
 </footer>
 </html>
