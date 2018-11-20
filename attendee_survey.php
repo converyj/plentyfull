d8be57c9990d33f56f2de8ed773d81eefc083e8c
converyj <42975408+converyj@users.noreply.github.com>
2018-11-20 18:11:29 -0500

    Update attendee_survey.php

--- attendee_survey.php
+++ attendee_survey.php
@@ -33,6 +33,7 @@ $stmt2->execute();
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<!-- Add website favicon -->
   <title>Plenty Full - Attendee Survey</title>
 </head>
 <body>
