a2a379d2ef42c89a352f1fc5d6cf52889f7c7e68
Jaime <jaimeconvery@gmail.com>
2018-11-20 18:07:03 -0500

    process_plannersurvey.php-v2

--- process_plannersurvey.php
+++ process_plannersurvey.php
@@ -3,17 +3,21 @@
 session_start(); 
 
 // check if user filled in all fields 
-if(!empty($_POST['firstName']) && !empty($_POST['lastName']) && !empty($_POST['email']) && !empty($_POST['city']) && !empty($_POST['country']) && !empty($_POST['deadline'])) {
-
+if(!empty($_POST['firstName']) && !empty($_POST['lastName']) && !empty($_POST['email']) 
+	&& !empty($_POST['street']) && !empty($_POST['city']) && !empty($_POST['country']) 
+	&& !empty($_POST['postal']) && !empty($_POST['ddlDate'])) {
+	
 	// get the form inputs 
 	$firstName = $_POST['firstName']; 
 	$lastName = $_POST['lastName']; 
 	$email = $_POST['email']; 
+	$street = $_POST['street']; 
 	$city = $_POST['city']; 
-	$country = $_POST['country']; 
-	$deadline = $_POST['deadline']; 
+	$country = $_POST['country'];
+	$postal = $_POST['postal'];  
+	$deadline = $_POST['ddlDate']; 
 } else {
-	header("Location: home.html");
+	header("Location: homepage.php");
 }
 
 $dsn = "mysql:host=localhost;dbname=converyj_plentyfull;charset=utf8mb4";
@@ -25,71 +29,70 @@ $pdo = new PDO($dsn, $dbusername, $dbpassword);
 // INSERT firstname, lastName, email into user table 
 $stmt = $pdo->prepare("
 						INSERT INTO `user` (`firstName`, `lastName`, `email`)
-						VALUES (‘$firstName’, ‘$lastName’, ‘$email’) "); 
+						VALUES ('$firstName', '$lastName', '$email') "); 
 $i = $stmt->execute();
 
-// get the userid that was auto-generated and save to SESSION to use later
-$userid = $pdo->lastInsertId('id'); 
-$_SESSION["userid"] = $userid;
+// check whether insert was successful, otherwise redirect to error page
+if ($i == 1) {
 
-// check whether insert was successful
-if ($i === 1) {
-	echo("Success"); 
-} else {
-	echo("Error: Could not insert the record");
-} 
+	// get the userid that was auto-generated and save to SESSION to use later
+	$userid = $pdo->lastInsertId('userid'); 
+	$_SESSION["userid"] = $userid;
 
-// INSERT city, country, role into admin table 
+	} else {
+		$message = "Error: Could not insert the record in user";
+		header("Location: error.php?message= " . $message);
+	} 
+
+// INSERT userid, role, city, country, street, postal code into admin table 
 $stmt = $pdo->prepare("
-						INSERT INTO `admin` (`userid`, `role`, `city`, `country`) 
-						VALUES ($userid, 1, ‘$city’, ‘$country’) "); 
+						INSERT INTO `admin` (`userid`, `role`, `city`, `country`, `streetName`, `postalCode`) 
+						VALUES ($userid, 1, '$city', '$country', '$street', '$postal') "); 
 $i = $stmt->execute();
 
-// check whether insert was successful
-if ($i === 1) {
-	echo("Success"); 
-} else {
-	echo("Error: Could not insert the record");
+// check whether insert was unsuccessful (redirect to error page), otherwise continue
+if ($i == 0) {
+	$message = "Error: Could not insert the record in admin";
+	header("Location: error.php?message= " . $message);
 } 
 
 // INSERT deadline into survey table 
 $stmt = $pdo->prepare("
 						INSERT INTO `survey` (`deadline`) 
-						VALUES (‘$deadline’) "); 
+						VALUES ('$deadline') "); 
 $i = $stmt->execute();
 
-// get the surveyid that was auto-generated and save in SESSION to use later 
-$surveyid = $pdo->lastInsertId('id'); 
-$_SESSION["surveyid"] = $surveyid;
+// check whether insert was successful, otherwise redirect to error page
+if ($i == 1) {
 
-// check whether insert was successful
-if ($i === 1) {
-	echo("Success"); 
+	 // get the surveyid that was auto-generated and save in SESSION to use later 
+	$surveyid = $pdo->lastInsertId('surveyid'); 
+	$_SESSION['surveyid'] = $surveyid;
 } else {
-	echo("Error: Could not insert the record");
+	$message = "Error: Could not insert the record in survey";
+	header("Location: error.php?message= " . $message);
 } 
 
 // INSERT dietary restrictions checkboxes into userdietary table 
 
 // check if any checkboxes are checked
-if (!empty($_POST["dietary"])) {
-	$dietary = $_POST['dietary']; 
+if (!empty($_POST["dietaryRestrictions"])) {
+	$dietary = $_POST['dietaryRestrictions']; 
 
-	// if checkbox or more was checked, loop through checkboxes
+	// if one or more was checked, loop through checkboxes and insert row in userdietary
 	if(count($dietary > 0)) {
 
-		foreach ($dietary as $dietCode) {
-			echo($dietCode);
+		foreach ($_POST['dietaryRestrictions'] as $dietCode) {
 			$stmt = $pdo->prepare("
 									INSERT INTO `userdietary` (`surveyid`, `userid`, `dietaryRestrictionCode`)
 									VALUES ($surveyid, $userid, $dietCode) "); 
 			$i = $stmt->execute();
 
-			// check whether insert was successful
-			if ($i === 1) {
-				echo("Success"); 
-			} else {
-				echo("Error: Could not insert the record");
+			// check whether insert was unsuccessful (redirect to error page), otherwise continue
+			if ($i == 0) {
+			} else { 
+				$message = "Error: Could not insert the record in userdietary";
+				header("Location: error.php?message= " . $message);
 			} 
 		}
 	}
@@ -98,27 +101,25 @@ if (!empty($_POST["dietary"])) {
 // INSERT allergy checkboxes into userallergy table 
 
 // check if any checkboxes are checked
-if (!empty($_POST["allergy"])) {
-	$allery = $_POST['allergy']; 
-}
+if (!empty($_POST["allergies"])) {
+	$allergy = $_POST['allergies']; 
 
-// if checkbox or more was checked, loop through checkboxes
-if(count($allergy > 0)) {
+	// if one or more was checked, loop through checkboxes and insert row in userallergy 
+	if(count($allergy > 0)) {
 
 		foreach ($allergy as $allergyCode) {
-			echo($allergyCode);
 			$stmt = $pdo->prepare("
 									INSERT INTO `userallergy` (`surveyid`, `userid`, `allergyCode`)
 									VALUES ($surveyid, $userid, $allergyCode) "); 
 			$i = $stmt->execute();
 
-			// check whether insert was successful
-			if ($i === 1) {
-				echo("Success"); 
-			} else {
-				echo("Error: Could not insert the record");
+			// check whether insert was unsuccessful (redirect to error page), otherwise continue
+			if ($i == 0) {
+				$message = "Error: Could not insert the record in userallergy";
+				header("Location: error.php?message= " . $message);
 			} 
 		}
+	}
 }
 
 // INSERT userid and surveyid into usersurvey table 
@@ -127,11 +128,12 @@ $stmt = $pdo->prepare("
 						VALUES ($userid, $surveyid) "); 
 $i = $stmt->execute();
 
-// check whether insert was successful
-if ($i === 1) {
-	echo("Success"); 
+// check whether insert was successful, otherwise redirect to error page
+if ($i == 1) {
+	header("Location: share_inputcode.php");
 } else {
-	echo("Error: Could not insert the record");
+	$message = "Error: Could not insert the record in usersurvey";
+	header("Location: error.php?message= " . $message);
 } 
 
 ?>
