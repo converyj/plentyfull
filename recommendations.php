962153537c10f80bdde7850fa18513d841e0ac22
CaptainAries <captainariesz@gmail.com>
2018-11-21 17:16:37 -0500

    image update

---
+++ recommendations.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Plenty Full -  Recommendations </title>
+</head>
+<body>
+  <!-- <a href=""><img src="" alt="logo"></a> -->
+<nav>
+  <ul>
+    <li><a href="explore.php">Explore</a></li>
+    <li><a href="inputCode.php">Input Code</a></li>
+    <li><a href="about.php">About</a></li>
+    <li><a href="login.php">Login</a></li>
+  </ul>
+</nav>
+
+<div id="bar">
+    <li>Discover</li>
+    <li>Newest</li>
+    <li>Popular</li>
+    <li><input type="search" name="search" /><input type="submit" value="Search" /></li>
+</div>
+
+</body>
+<footer>
+  <a href="mailto:info@plentyfull.com">info@plentyfull.com</a>
+  <br />
+  <a href="https://www.instagram.com/"><img src="images/pesc.png" alt="ins" /></a>
+  <a href="https://www.facebook.com/"><img src="images/facebook.png" alt="facebook" /></a>
+  <a href="https://www.twitter.com/"><img src="images/twitter.png" alt="twitter" /></a>
+</footer>
+</html>
