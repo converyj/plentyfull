d69d0977a5a7bff1444314dec8d11af0bf12da61
CaptainAries <captainariesz@gmail.com>
2018-11-21 15:26:58 -0500

    update

--- attendee_survey.php
+++ attendee_survey.php
@@ -60,7 +60,7 @@ $stmt2->execute();
       <?php
       while ($row = $stmt1->fetch()) {
       ?>
-        <input type="checkbox" name="dietaryRestrictions[]" value="<?php echo($row['code']); ?>" /><?php echo($row['value']); ?> <?php echo($row['image']); ?>
+        <input type="checkbox" name="dietaryRestrictions[]" value="<?php echo($row['code']); ?>" /><?php echo($row['value']); ?> <?php echo($row['image']); ?> <img src="images/<?php echo($row['image']); ?>" alt="image" />
       <?php } ?>
     </p>
     <p>
@@ -69,7 +69,7 @@ $stmt2->execute();
       <?php
       while ($row = $stmt2->fetch()) {
       ?>
-        <input type="checkbox" name="allergies[]" value="<?php echo($row['code']); ?>" /><?php echo($row['value']); ?> <?php echo($row['image']); ?>
+        <input type="checkbox" name="allergies[]" value="<?php echo($row['code']); ?>" /><?php echo($row['value']); ?> <?php echo($row['image']); ?> <img src="images/<?php echo($row['image']); ?>" alt="image" />
      <?php } ?>
     </p>
     <p>
