ce5d4cbbd4860d6df5304cb6dc598267c810df74
Jaime <jaimeconvery@gmail.com>
2018-12-03 09:23:41 -0500

    survey results for all users

--- full-results.php
+++ full-results.php
@@ -1,38 +1,27 @@
 <?php 
+
 session_start();
+
+$surveyid = $_SESSION['surveyid']; 
+
 $dsn = "mysql:host=localhost;dbname=converyj_plentyfull;charset=utf8mb4";
 $dbusername = "converyj";
 $dbpassword = "HUgT86Fga#97";
 $pdo = new PDO($dsn, $dbusername, $dbpassword);
 
-// SELECT all users (this is attendees right?)
+// SELECT all users from survey
 $stmt = $pdo->prepare("
                         SELECT `userid`, `firstName`, `lastName`, `email`
-                        FROM `user`");
+                        FROM `user`
+                        WHERE `user`.`userid` IN 
+                                                  (SELECT `userid` 
+                                                  FROM `usersurvey`
+                                                  WHERE `usersurvey`.`surveyid` = $surveyid); "); 
 $stmt->execute();
-
-// SELECT all the dietary images
-$stmt1 = $pdo->prepare("
-                        SELECT `image`, `value`, `code`
-                        FROM `dietallergyvalue`
-                        WHERE `dietallergyvalue`.`type` = 'D'");
-$stmt1->execute();
-// SELECT all the allergy images
-$stmt2 = $pdo->prepare("
-                        SELECT `image`, `value`, `code`
-                        FROM `dietallergyvalue`
-                        WHERE `dietallergyvalue`.`type` = 'A'");
-$stmt2->execute();
-
-// SELECT comments
-// $stmt3 = $pdo->prepare("
-//                         SELECT `userid`, `comment`
-//                         FROM `comment` ");
-// $stmt3->execute();
 
 ?>
 
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
@@ -46,79 +35,117 @@ $stmt2->execute();
   <title>Plenty Full - Full Results</title>
 </head>
 <body>
-		
-<div class="at">
-  <ul>
-  <a href="homepage.php" class="main-logo"><img src="images/logo-white.png" width="20%"></a>
-    <li><a href="explore.php">Explore</a></li>
-    <li><a href="inputCode.php">Input Code</a></li>
-    <li><a href="about.php">About</a></li>
-    <li><a href="login.php">Login</a></li>
-  </ul>
-</div>
+  <div class="at"> 
+    <ul>
+      <a href="homepage.php" class="main-logo"><img src="images/logo-white.png" width="20%"></a>
+      <li><a href="explore.php">Explore</a></li>
+      <li><a href="inputCode.php">Input Code</a></li>
+      <li><a href="about.php">About</a></li>
+      <li><a href="login.php">Login</a></li>
+    </ul>
+  </div>
+  <div class="container">
+    <p class="start">Full Results</p>
 
-<div class="container">
-		<p class="start">Full Results</p>
-
-<?php
-      //show records (process results)
-      while($row = $stmt->fetch()) {     
-        //echo($row["email"]); //or $row[0];
-        ?><div>
-          <a href="edit.php">Edit</a></span>
-          <a href="delete.php">Delete</a></span>
-          <p>First Name: <?php echo($row["firstName"]); ?></p>
-          <p>Last Name: <?php echo($row["lastName"]); ?></p>
-          <p>Email: <?php echo($row["email"]); ?></p>
-          <?php } ?>
-      
-<?php
-      while($row = $stmt1->fetch()) {
+        <?php
+        //show results for each user in the survey
+        while($row = $stmt->fetch()) {    
         ?>
-         <p>Dietary:</p>
-           <div class="dietdiv">
-          <label for="<?php echo($row['code']); ?>">
-            <img src="images/<?php echo($row['image']); ?>" class="image" alt="image" />
-            </label>
-      <?php } ?>
-<?php
-      while($row = $stmt2->fetch()) {
+          <div>
+            <a href="edit.php">Edit</a></span>
+            <a href="delete.php">Delete</a></span>
+            <p>First Name: <?php echo($row["firstName"]); ?></p>
+            <p>Last Name: <?php echo($row["lastName"]); ?></p>
+            <p>Email: <?php echo($row["email"]); ?></p>
+            <?php
+            $userid = $row['userid']; 
+
+            // call function to get user details
+            getUserDetails($pdo, $surveyid, $userid); 
+            ?>
+          </div>
+        <?php
+        }
         ?>
-         <p>Allergens:</p>
-           <div class="dietdiv">
-          <label for="<?php echo($row['code']); ?>">
-            <img src="images/<?php echo($row['image']); ?>" class="image" alt="image" />
-            </label>
-       <?php } ?>
-
-
-
-        </div>
-      
-      <br>
-
-<!-- need to find the wasim notes that showed a list of everyone's info pulled from the database -->
-
-   <!--    <form action="confirm-update.php" method="POST">  
-  
-  <p>id: <?php echo($row["id"]); ?></p>
-  <input type="hidden" value="<?php echo($row["id"]); ?>" name="id"/>
-  <p>First Name: <input type='text' name='firstName' value="<?php echo($row["firstName"]); ?>"/></p>
-  <p>Last Name: <input type='text' name='lastName' value="<?php echo($row["lastName"]); ?>"/></p>
-  <p>email: <input type='text' name='email' value="<?php echo($row["email"]); ?>"/></p>
-  <p>Date of Birth: <input type='text' name='dob' value="<?php echo($row["dob"]); ?>"/></p>
-  <input type='submit'/> 
-</form>
- -->
-
-</div>
- <script src="js/main.js"></script>
+  </div>	
+  <script src="js/main.js"></script>
 </body>
 <footer> 
-    <a href="mailto:info@plentyfull.com" class="email">info@plentyfull.com</a>
-    <br />
-    <a href="https://www.twitter.com/"><img src="images/twitter.png" width="3%" alt="twitter" /></a>
-    <a href="https://www.facebook.com/"><img src="images/facebook.png" width="3%" alt="facebook" /></a>
-    <a href="https://www.instagram.com/"><img src="images/ig.png" width="3%" alt="ins" /></a>
+  <a href="mailto:info@plentyfull.com" class="email">info@plentyfull.com</a>
+  <br />
+  <a href="https://www.twitter.com/"><img src="images/twitter.png" width="3%" alt="twitter" /></a>
+  <a href="https://www.facebook.com/"><img src="images/facebook.png" width="3%" alt="facebook" /></a>
+  <a href="https://www.instagram.com/"><img src="images/ig.png" width="3%" alt="ins" /></a>
 </footer>
 </html>
+
+<?php 
+function getUserDetails($pdo, $surveyid, $userid) {
+
+  // SELECT all dietary restrictions for the user
+  $stmt1 = $pdo->prepare("
+                          SELECT `value`
+                          FROM `userdietary`
+                          INNER JOIN `dietallergyvalue` ON `userdietary`.`dietaryRestrictionCode` = `dietallergyvalue`.`code` 
+                          WHERE `userdietary`.`surveyid` = $surveyid 
+                          AND `userdietary`.`userid` = $userid 
+                          AND `dietallergyvalue`.`type` = 'D'"); 
+  $stmt1->execute();
+
+  // SELECT all allergies for the user
+  $stmt2 = $pdo->prepare("
+                          SELECT `value`
+                          FROM `userallergy`
+                          INNER JOIN `dietallergyvalue` ON `userallergy`.`allergyCode` = `dietallergyvalue`.`code` 
+                          WHERE `userallergy`.`surveyid` = $surveyid 
+                          AND `userallergy`.`userid` = $userid 
+                          AND `dietallergyvalue`.`type` = 'A'");
+  $stmt2->execute();
+
+  // SELECT comments for the user 
+  $stmt3 = $pdo->prepare("
+                          SELECT `comment`
+                          FROM `comment` 
+                          WHERE `comment`.`userid` = $userid");
+  $stmt3->execute();
+
+  // display dietary details 
+  $numRows = 0; 
+  while ($row1 = $stmt1->fetch()) {
+    if ($numRows == 0) {
+    ?>
+      <p>Dietary:
+    <?php
+    }
+    $numRows++; 
+    echo($row1['value']) . " "; 
+  }
+  ?>
+    </p> 
+  
+  <?php
+
+  // display allergy details 
+  $numRows = 0; 
+  while ($row2 = $stmt2->fetch()) {
+    if ($numRows == 0) {
+    ?>
+      <p>Allergy:
+    <?php
+    }
+    $numRows++; 
+    echo($row2['value']) . " "; 
+  }
+  ?>
+    </p> 
+
+  <?php
+
+  // display comments
+  if ($row3 = $stmt3->fetch()) {
+  ?>
+    <p>Other: <?php echo($row3['comment']); ?></p>
+  <?php 
+  }
+}
+?>
