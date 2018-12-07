2348433ff24e2c693e80bde30200544f7fa7c427
CaptainAries <captainariesz@gmail.com>
2018-12-07 17:20:06 -0500

    changes

--- full-results.php
+++ full-results.php
@@ -1,8 +1,8 @@
-<?php 
+<?php
 
 session_start();
 
-$surveyid = $_SESSION['surveyid']; 
+$surveyid = $_SESSION['surveyid'];
 
 $dsn = "mysql:host=localhost;dbname=converyj_plentyfull_new;charset=utf8mb4";
 $dbusername = "converyj";
@@ -13,10 +13,10 @@ $pdo = new PDO($dsn, $dbusername, $dbpassword);
 $stmt = $pdo->prepare("
                         SELECT `userid`, `firstName`, `lastName`, `email`
                         FROM `user`
-                        WHERE `user`.`userid` IN 
-                                                  (SELECT `userid` 
+                        WHERE `user`.`userid` IN
+                                                  (SELECT `userid`
                                                   FROM `usersurvey`
-                                                  WHERE `usersurvey`.`surveyid` = $surveyid); "); 
+                                                  WHERE `usersurvey`.`surveyid` = $surveyid); ");
 $stmt->execute();
 
 ?>
@@ -29,36 +29,41 @@ $stmt->execute();
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
   <!-- <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"> -->
-  <link rel="stylesheet" type="text/css" href="css/main2.css">
-
+  <link rel="stylesheet" type="text/css" href="css/main3forOrange.css">
+  <link rel="stylesheet" media="screen and (max-width: 640px)" href="css/small.css" />
+  <link rel="icon" href="images/favicon.ico" />
   <!-- plentyfull favicon -->
   <title>Plenty Full - Full Results</title>
 </head>
 <body>
-  <div class="at"> 
-    <ul>
-      <a href="homepage.php" class="main-logo"><img src="images/logo-white.png" width="20%"></a>
-      <!-- <li><a href="explore.php">Explore</a></li> -->
-      <li><a href="inputCode.php">Input Code</a></li>
-      <li><a href="about.php">About</a></li>
-      <!-- if already logged in, change navigation  -->
-    <?php 
-    if (isset($_SESSION['logged-in'])) {
-    ?>
-        <li>
-          <a href="logout.php">Logout</a>
-        </li>
-      <?php 
-      }
-      ?>
-    </ul>
-  </div>
+   <nav>
+      <a href="homepage.php" id="main-logo"></a>
+          <div class="at">
+            <a href="#" id="menu-icon"></a>
+            <ul>
+              <!-- <a href="homepage.php" class="main-logo"><img src="images/logo-white.png" width="20%"></a> -->
+              <!-- <li><a href="explore.php">Explore</a></li> -->
+              <li><a href="inputCode.php">Input Code</a></li>
+              <li><a href="about.php">About</a></li>
+              <!-- if already logged in, change navigation  -->
+            <?php
+            if (isset($_SESSION['logged-in'])) {
+            ?>
+                <li>
+                  <a href="logout.php">Logout</a>
+                </li>
+              <?php
+              }
+              ?>
+            </ul>
+          </div>
+      </nav>
   <div class="container">
     <p class="start">Full Results</p>
   <div class="survey-people">
         <?php
         //show results for each user in the survey
-        while($row = $stmt->fetch()) {    
+        while($row = $stmt->fetch()) {
         ?>
           <div>
             <a class="edit-delete" href="edit.php?id=<?php echo($row["userid"]); ?>">Edit</a></span>
@@ -67,20 +72,20 @@ $stmt->execute();
             <p>Last Name: <?php echo($row["lastName"]); ?></p>
             <p>Email: <?php echo($row["email"]); ?></p>
             <?php
-            $userid = $row['userid']; 
+            $userid = $row['userid'];
 
             // call function to get user details
-            getUserDetails($pdo, $surveyid, $userid); 
+            getUserDetails($pdo, $surveyid, $userid);
             ?>
             <br>
           </div>
         <?php
         }
         ?>
-  </div>	
+  </div>
   <script src="js/main.js"></script>
 </body>
-<footer> 
+<footer>
   <a href="mailto:info@plentyfull.com" class="email">info@plentyfull.com</a>
   <br />
   <a href="https://www.twitter.com/"><img src="images/twitter.png" width="30px" alt="twitter" /></a>
@@ -89,65 +94,65 @@ $stmt->execute();
 </footer>
 </html>
 
-<?php 
+<?php
 function getUserDetails($pdo, $surveyid, $userid) {
 
   // SELECT all dietary restrictions for the user
   $stmt1 = $pdo->prepare("
                           SELECT `value`
                           FROM `userdietary`
-                          INNER JOIN `dietallergyvalue` ON `userdietary`.`dietaryRestrictionCode` = `dietallergyvalue`.`code` 
-                          WHERE `userdietary`.`surveyid` = $surveyid 
-                          AND `userdietary`.`userid` = $userid 
-                          AND `dietallergyvalue`.`type` = 'D'"); 
+                          INNER JOIN `dietallergyvalue` ON `userdietary`.`dietaryRestrictionCode` = `dietallergyvalue`.`code`
+                          WHERE `userdietary`.`surveyid` = $surveyid
+                          AND `userdietary`.`userid` = $userid
+                          AND `dietallergyvalue`.`type` = 'D'");
   $stmt1->execute();
 
   // SELECT all allergies for the user
   $stmt2 = $pdo->prepare("
                           SELECT `value`
                           FROM `userallergy`
-                          INNER JOIN `dietallergyvalue` ON `userallergy`.`allergyCode` = `dietallergyvalue`.`code` 
-                          WHERE `userallergy`.`surveyid` = $surveyid 
-                          AND `userallergy`.`userid` = $userid 
+                          INNER JOIN `dietallergyvalue` ON `userallergy`.`allergyCode` = `dietallergyvalue`.`code`
+                          WHERE `userallergy`.`surveyid` = $surveyid
+                          AND `userallergy`.`userid` = $userid
                           AND `dietallergyvalue`.`type` = 'A'");
   $stmt2->execute();
 
-  // SELECT comments for the user 
+  // SELECT comments for the user
   $stmt3 = $pdo->prepare("
                           SELECT `comment`
-                          FROM `comment` 
+                          FROM `comment`
                           WHERE `comment`.`userid` = $userid");
   $stmt3->execute();
 
-  // display dietary details 
-  $numRows = 0; 
+  // display dietary details
+  $numRows = 0;
   while ($row1 = $stmt1->fetch()) {
     if ($numRows == 0) {
     ?>
       <p>Dietary:
     <?php
     }
-    $numRows++; 
-    echo($row1['value']) . " "; 
+    $numRows++;
+    echo($row1['value']) . " ";
   }
   ?>
-    </p> 
-  
+    </p>
+
   <?php
 
-  // display allergy details 
-  $numRows = 0; 
+  // display allergy details
+  $numRows = 0;
   while ($row2 = $stmt2->fetch()) {
     if ($numRows == 0) {
     ?>
       <p>Allergy:
     <?php
     }
-    $numRows++; 
-    echo($row2['value']) . " "; 
+    $numRows++;
+    echo($row2['value']) . " ";
   }
   ?>
-    </p> 
+    </p>
 
   <?php
 
@@ -155,7 +160,7 @@ function getUserDetails($pdo, $surveyid, $userid) {
   if ($row3 = $stmt3->fetch()) {
   ?>
     <p>Other: <?php echo($row3['comment']); ?></p>
-  <?php 
+  <?php
   }
 }
 ?>
